13c2e6a9f630cee47845e9a8c5043a5bda9c26fe
huu my <hhmy1995@gmail.com>
2022-12-24 22:16:12 +0700

    created database

--- library/__init__.py
+++ library/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask, request, Blueprint
 from .books.controller import books
+from .extension import db
+from .model import Students, Books, Author, Category, Borrows
+import os
+
+
+def create_db():
+    if not os.path.exists("library/library.db"):
+        db.create_all()
+        print("Created DB!!!")
 
 def create_app(config_file = "config.py"):
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
+    db.init_app(app)
+    with app.app_context():
+        create_db()
     app.register_blueprint(books)
     return app
---
+++ library/model.py
@@ -0,0 +1,58 @@
+from .extension import db
+
+class Students(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    birth_date = db.Column(db.Date)
+    gender = db.Column(db.String(10))
+    class_name = db.Column(db.String(10))
+
+    def __init__(sefl, name, birth_date, gender, class_name):
+        sefl.name = name
+        sefl.birth_date = birth_date
+        sefl.gender = gender
+        sefl.class_name = class_name
+
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    page_count = db.Column(db.Integer)
+    author_id = db.Column(db.String(10))
+    category_id = db.Column(db.String(10))
+
+    def __init__(sefl, name, page_count, author_id, category_id):
+        sefl.name = name
+        sefl.page_count = page_count
+        sefl.author_id = author_id
+        sefl.category_id = category_id
+
+
+class Borrows(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    student_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    borrow_date = db.Column(db.Date)
+    return_date = db.Column(db.Date)
+
+    def __init__(sefl, book_id, student_id, borrow_date, return_date):
+        sefl.book_id =book_id
+        sefl.student_id =student_id
+        sefl.borrow_date =borrow_date
+        sefl.return_date =return_date
+        
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), unique = True, nullable = False)
+    def __init__(seft, name):
+        seft.name = name
+        
+        
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), unique = True, nullable = False)
+    def __init__(seft, name):
+        seft.name = name
+    
+        
