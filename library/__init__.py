379f5a7bd15c791611c90ca448f9369cca21f710
huu my <hhmy1995@gmail.com>
2022-12-24 09:26:41 +0700

    environment initialization and run the get all book function(test)

--- library/__init__.py
+++ library/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, Blueprint
+from .books.controller import books
 
-def create_app():
+def create_app(config_file = "config.py"):
     app = Flask(__name__)
+    app.config.from_pyfile(config_file)
+    app.register_blueprint(books)
     return app
