522458f4e0a3cab037cb373856e9452a679d12b0
huu my <hhmy1995@gmail.com>
2023-01-05 16:25:50 +0700

    add image picker package

---
+++ library_app/lib/components/image_picker_button.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:library_app/components/default_button.dart';
+
+class ImagePickerButton extends StatelessWidget {
+  const ImagePickerButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultButton(
+      press: () {},
+    );
+  }
+}
